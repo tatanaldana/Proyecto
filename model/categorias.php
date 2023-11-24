37380a5d40a49955cce932b4aa4db86c417ac4db
luis <lacortes47@soy.sena.edu.co>
2023-11-23 23:01:34 -0500

    update models
    cambios en models

--- model/categorias.php
+++ model/categorias.php
@@ -23,9 +23,9 @@ class categorias extends Conexion
     {
         $conectar = parent::conexion();
         parent::set_names();
-        $sql = "SELECT * FROM categorias WHERE id_categoria = ?";
+        $sql = "SELECT * FROM categorias WHERE id_categoria = :id_categoria";
         $sql = $conectar->prepare($sql);
-        $sql->bindValue(1, $id_categoria);
+        $sql->bindParam(':id_categoria', $id_categoria);
         $sql->execute();
         return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
     }
@@ -36,10 +36,10 @@ class categorias extends Conexion
     {
         $conectar = parent::conexion();
         parent::set_names();
-        $sql = "INSERT INTO categorias(id_categoria,nombre_cat,,estado) VALUES (NULL,?,?,?);";
+        $sql = "INSERT INTO categorias(id_categoria,nombre_cat,estado) VALUES (NULL,:id_categoria,:nombre_cat,:estado);";
         $sql = $conectar->prepare($sql);
-        $sql->bindValue(1, $nombre_cat);
-        $sql->bindValue(3, $estado);
+        $sql->bindParam(':nombre_cat', $nombre_cat);
+        $sql->bindParam(':estado', $estado);
 
         $sql->execute();
 
@@ -53,25 +53,25 @@ class categorias extends Conexion
         $conectar = parent::conexion();
         parent::set_names();
         $sql = " UPDATE categorias set
-        nombre_cat = ?,
-        estado = ?,
+        nombre_cat = :nombre_cat,
+        estado = :estado,
         WHERE
-        id_categoria =?";
+        id_categoria =:id_categoria";
         $sql = $conectar->prepare($sql);
-        $sql->bindValue(1, $nombre_cat);
-        $sql->bindValue(3, $estado);
+        $sql->bindParam(':nombre_cat', $nombre_cat);
+        $sql->bindParam(':estado', $estado);
         $sql->execute();
         return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
     }
 
     //eliminar categoria
-    public function eliminar_categorias($instru_id)
+    public function eliminar_categorias($id_categoria)
     {
         $conectar = parent::conexion();
         parent::set_names();
-        $sql = "DELETE FROM categorias WHERE id_categoria = ?";
+        $sql = "DELETE FROM categorias WHERE id_categoria = :id_categoria";
         $sql = $conectar->prepare($sql);
-        $sql->bindValue(1, $instru_id);
+        $sql->bindParam(':id_categoria', $id_categoria);
         $sql->execute();
         return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
     }
--- model/promocion.php
+++ model/promocion.php
@@ -64,7 +64,7 @@ class Promocion extends Conexion
         parent::set_names();
         $sql="DELETE FROM promocion WHERE id_promo = :id_promo";
         $sql=$conectar->prepare($sql);
-        $sql->bindParam(1, $id_promo);
+        $sql->bindParam(':id_promo', $id_promo);
         $sql->execute();
         return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
     }
