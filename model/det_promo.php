5b3230ceaee474d75ca0e9dc35751d15da050073
tatanaldana <jbojhonatanaldana@gmail.com>
2023-11-23 20:38:43 -0500

    Create det_promo.php
    Se crea el archivo model det_promo.php y se cambian las sentencias para el uso de marcadores y de la función BindParam.

---
+++ model/det_promo.php
@@ -0,0 +1,75 @@
+<?php
+class Det_promo extends Conexion{
+    //se obtiene todos los registros de la tabla det_prom
+    public function get_det_promo(){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="SELECT * FROM det_promo";
+        $sql=$conectar->prepare($sql);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+//Se obtiene el detalle de una promoción.
+    public function get__det_promo_x_idpromo($idpromo){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="SELECT * FROM det_promo WHERE idpromo = :idpromo";
+        $sql=$conectar->prepare($sql);
+        $sql->bindParam(':idpromo', $idpromo);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+//se inserta el detalle de la promoción
+    public function insert_det_promo($nom_prod,$pre_prod,$cantidad,$descuento,$subtotal,$total,$promocion_idpromo){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="INSERT INTO det_promo(idpromo,nom_prod,pre_prod,cantidad,descuento,subtotal,total,promocion_idpromo) VALUES (Null,:nom_pro,:pre_pro,:cantidad,:descuento,:subtotal,:total,:promocion_idpromo);";
+        $sql=$conectar->prepare($sql);
+        $sql->bindParam(':nom_prod', $nom_prod);
+        $sql->bindParam(':pre_prod', $pre_prod);
+        $sql->bindParam(':cantidad', $cantidad);
+        $sql->bindParam(':descuento', $descuento);
+        $sql->bindParam('subtotal', $subtotal);
+        $sql->bindParam('total', $total);
+        $sql->bindParam('promocion_idpromo', $promocion_idpromo);
+        $sql->execute();
+        
+        
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+//Se actualiza el detalle de una promoción
+    public function update_det_promo($idpromo,$nom_prod,$pre_prod,$cantidad,$descuento,$subtotal,$total,$promocion_idpromo){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql=" UPDATE det_promo set nom_prod = :nom_pro,pre_prod =:pre_pro, cantidad = :cantidad, descuento = :descuento,
+        subtotal = :subtotal, total = :total, promocion_idpromo = :promocion_idpromo WHERE idpromo =:idpromo";
+        $sql=$conectar->prepare($sql);
+        $sql->bindParam(':nom_prod', $nom_prod);
+        $sql->bindParam(':pre_prod', $pre_prod);
+        $sql->bindParam(':cantidad', $cantidad);
+        $sql->bindParam(':descuento', $descuento);
+        $sql->bindParam(':$subtotal', $subtotal);
+        $sql->bindParam(':total', $total);
+        $sql->bindParam(':promocion_idpromo', $promocion_idpromo);
+        $sql->bindParam(':idpromo', $idpromo);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+//Se elimina una el detalle de una promoción.
+    public function eliminar_det_promo($idpromo){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="DELETE FROM det_promo WHERE idpromo = :idpromo";
+        $sql=$conectar->prepare($sql);
+        $sql->bindParam(':idpromo', $idpromo);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+
+
+
+}  
+?>
