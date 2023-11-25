7b6bc77486bb700282ac8919463edfec0151cb44
luis <lacortes47@soy.sena.edu.co>
2023-11-24 20:03:00 -0500

    update
    creacion de  model y controller proveedores

---
+++ controller/apicontroller/provee.php
@@ -0,0 +1,40 @@
+<?php
+header('content-Type: aplication/json');
+
+require_once("../../model/conexion.php");
+require_once("../../model/provee.php");
+$provee = new provee();
+
+$body = json_decode(file_get_contents("php://input"), true);
+
+switch($_GET["op"]){
+
+    case "GetAll":
+        $datos=$provee->get_provee();
+        echo json_encode($datos);
+        break;
+
+        case "GetId":
+            $datos=$provee->get_provee_x_idproveedor($body["idproveedor"]);
+            echo json_encode($datos);
+            break;
+
+            case "Insert":
+                $datos=$provee->insert_provee($body["idproveedor"],$body["nom_proveedor"], $body["telefono_proveedor"],$body["direccion_proveedor"]);
+                echo json_encode("Insert Correto");
+                break;
+
+                    case "Update":
+                        $datos=$provee->update_provee($body["idproveedor"],$body["nom_proveedor"], $body["telefono_proveedor"],$body["direccion_proveedor"]);
+                         echo json_encode("Insert Correto");
+                        break;
+                
+                            case "Eliminar":
+                                $datos = $provee->delete_provee($body["idproveedor"]);
+                                echo json_encode("Eliminado correctamente");
+                                break;  
+
+                       
+
+}
+?>
---
+++ model/provee.php
@@ -0,0 +1,67 @@
+<?php
+class Det_promo extends Conexion{
+    //se obtiene todos los registros de la tabla det_prom
+    public function get_provee(){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="SELECT * FROM provee";
+        $sql=$conectar->prepare($sql);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+//Se obtiene el detalle de una promoción.
+    public function get_provee_x_idproveedor($idproveedor){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="SELECT * FROM provee WHERE idproveedor = :idproveedor";
+        $sql=$conectar->prepare($sql);
+        $sql->bindParam(':idproveedor', $idproveedor);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+//se inserta el detalle de la promoción
+    public function insert_provee($nom_proveedor,$telefono_proveedor,$direccion_proveedor){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="INSERT INTO provee(idproveedor,nom_proveedor,telefono_proveedor,direccion_proveedor) VALUES (Null,:nom_proveedor,:telefono_proveedor,:direccion_proveedor);";
+        $sql=$conectar->prepare($sql);
+        $sql->bindParam(':nom_proveedor', $nom_proveedor);
+        $sql->bindParam(':telefono_proveedor', $telefono_proveedor);
+        $sql->bindParam(':direccion_proveedor', $direccion_proveedor);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+//Se actualiza el detalle de una promoción
+    public function update_provee($idproveedor,$nom_proveedor,$telefono_proveedor,$direccion_provedor){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql=" UPDATE provee set
+        nom_proveedor = :nom_proveedor,
+        telefono_proveedor =:telefono_proveedor,
+        direccion_proveedor = :direccion_proveedor
+        WHERE idproveedor =:idproveedor";
+        $sql=$conectar->prepare($sql);
+        $sql->bindParam(':nom_proveedor', $nom_proveedor);
+        $sql->bindParam(':telefono_proveedor', $telefono_proveedor);
+        $sql->bindParam(':direccion_proveedor', $direccion_provedor);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+//Se elimina una el detalle de una promoción.
+    public function delete_provee($idproveedor){
+        $conectar= parent::conexion();
+        parent::set_names();
+        $sql="DELETE FROM provee WHERE idproveedor = :idproveedor";
+        $sql=$conectar->prepare($sql);
+        $sql->bindParam(':idproveedor', $idproveedor);
+        $sql->execute();
+        return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+
+
+
+}  
+?>
