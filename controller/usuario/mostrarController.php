771788e4c2fc6d794a4b42753929a7ecee9d58d1
tatanaldana <jbojhonatanaldana@gmail.com>
2023-11-30 12:47:34 -0500

    Create mostrarController.php
    se crea controlador para la visualización de los registros de la tabla usuarios cuando se ingresa un criterio de búsqueda abierta

---
+++ controller/usuario/mostrarController.php
@@ -0,0 +1,25 @@
+<?php
+
+if (isset($_POST['buscar_php'])) {
+    $buscar = $_POST['buscar_php'];
+    // Aquí podrías sanitizar $buscar antes de utilizarlo en la consulta
+
+     # Incluimos la clase usuario
+    require_once('../../model/usuario.php');
+     # Creamos un objeto de la clase usuario
+    $usuario = new Usuario();
+    # Llamamos al metodo  para realizar la consulta en la base de datos
+    $resultado = $usuario->ver_usuario($buscar);
+    if ($resultado) {
+        #GUARDAMOS LA VARIABLE EN FORMATO json PARA PODER ENVIAR LOS DATOS DE LA CONSULTA POR MEDIO DEL AJAX
+        $json_response = json_encode($resultado);
+        echo $json_response;
+    } else {
+        $error = array('error' => 'No se encontraron datos del usuario');
+        echo json_encode($error);
+    }
+} else {
+    $error = array('error' => 'Parámetros incorrectos');
+    echo json_encode($error);
+}
+?>
