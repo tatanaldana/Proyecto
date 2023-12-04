aed56f5076d66d93a9cb263a5b9c1857bbc695de
Eduardo <daniel.gamb98@hotmail.com>
2023-12-04 18:59:00 -0500

    se crea esta carpeta para guardar los formularios de la parte del usuario

---
+++ view/user/forms/form_pagodatos.php
@@ -0,0 +1,67 @@
+<div class="container"> 
+    <h1 class="center" style="color:var(--primario)">Informacion del Cliente</h1>
+
+
+    <table cellspacing ="20" cellpadding = "50">
+    <tr>
+
+        <td style=" margin-right: 4rem"><b>Documento</b></td>
+
+        <td align="center"><h6 id = "doc"></h6></td>   
+
+    </tr>
+
+    <tr>
+
+        <td><b>Nombre</b></td>
+
+        <td align="center"><h6 id = "nombre"></h6></td>
+
+    </tr>
+
+    <tr>
+
+        <td><b>Correo</b></td>
+
+        <td align="center"><h6 id = "email"></h6></td>
+
+    </tr>
+
+    <tr>
+
+        <td><b>Telefono</b></td>
+
+        <td align="center"><h6 id = "tel"></h6></td>
+
+    </tr>
+
+    <tr>
+
+    <tr>
+
+        <td><b>Domicilio</b></td>
+
+        <td align="center"><h6 id = "direccion"></h6></td>
+
+    </tr>
+
+    
+</table>
+
+
+  <div class="center">
+  <a href="../forms/form_metodopago.php" class=""><button type="buttom" class="btn regular-button mt-5" style="background: var(--primario); color: white;"> Continuar </button></a>
+    
+  </div>
+  
+
+
+<script src="../../public/js/jquery.js"></script>
+
+<script src="../../public/js/sweetalert.min.js"></script>
+<!-- Js usuarios -->
+<script src="../../public/js/usuario.js"></script>
+<!-- Js botones -->
+<script src="../../public/js/buttons.js"></script>
+
+<script src="../../public/js/dates.js"></script>
---
+++ view/user/forms/form_perfil.php
@@ -0,0 +1,37 @@
+<div class='container '>
+
+<div class='perfil mx-auto'>
+
+<!-- Titulo -->
+  <h1 class='text-center my-5'>Perfil de" . ' ' . ucfir. "</h1>
+
+  <div class='row'>
+
+    <div class='col-sm-5 center'>
+    <input type='file'><img src='header/img/logo.jpg' alt='Imagen perfil' class='rounded-circle' width='100px' height='100px'>
+
+    </div>
+
+    <div class='col-sm-5'>
+      <h4><b>Nombre:</b>  <h6 id = "nombre"></h6>
+      <h4><b>Apellido:</b>  <h6 id = "apellido"></h6>
+      <h4><b>Correo:</b>  <h6 id = "email"></h6>
+      <h4><b>Genero:</b> <h6 id = "genero"></h6>
+      <h4><b>Fecha de Nacimiento:</b><h6 id = "fecha_naci"></h6>
+      <h4><b>Tipo de documento:</b> <h6 id = "tipo_doc"></h6>
+      <h4><b>Documento de Identidad:</b> <h6 id = "doc"></h6>
+      <h4><b>Telefono:</b>  <h6 id = "tel"></h6>
+      <h4><b>Direccion:</b>  <h6 id = "direccion"></h6>
+    </div>
+
+
+    <div class='col-sm-2'>
+      <a href='configuracion/editar.php?id=" <?php $_SESSION['doc'] ?>"'><button class='btn-event' style = border:none ;><i class='fa fa-edit' style='font-size:20px'></i></button></a>
+            <div class= 'vr'></div>
+            <a href='configuracion/eliminar.php?id=" <?php $_SESSION['doc']  ?> "'><button class='btn-enviar' style = border:none ;><i class='fa fa-trash' style='font-size:20px'></i></button></a>
+        </div>
+      
+      </div>
+
+    </div>
+
