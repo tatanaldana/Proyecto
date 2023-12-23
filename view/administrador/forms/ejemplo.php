5dce09ca6e7b2d1773d3f184776ed2979ef95e72
luis <lacortes47@soy.sena.edu.co>
2023-12-22 19:41:48 -0500

    luis
    creacion de cat,js y actualizacion ejemplo.php

--- view/administrador/forms/ejemplo.php
+++ view/administrador/forms/ejemplo.php
@@ -1,46 +1,18 @@
-<script>
-    function abrirform() {
-        document.getElementById("formregistrar").style.display = "block";
-    }
-
-    function cancelarform() {
-        document.getElementById("formregistrar").style.display = "none";
-    }
-
-    function enviarFormulario() {
-        var nombreCat = document.getElementById("nombre_cat").value;
-
-        // Validar campos si es necesario
-
-        // Crear objeto FormData para enviar datos
-        var formData = new FormData();
-        formData.append('nombre_cat', nombreCat);
-
-        // Crear objeto XMLHttpRequest
-        var xhr = new XMLHttpRequest();
-
-        // Configurar la solicitud
-        xhr.open('POST', '../gestion/categorias/agregar_cat.php', true);
-
-        // Definir la función de devolución de llamada
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                // Procesar la respuesta del servidor si es necesario
-                console.log(xhr.responseText);
-                // Puedes realizar acciones adicionales según la respuesta del servidor
-            } else {
-                // Manejar errores
-                console.error('Error en la solicitud AJAX');
-            }
-        };
-
-        // Enviar la solicitud con los datos del formulario
-        xhr.send(formData);
-    }
-</script>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gregar categorias</title>
+    <!--  esto es para incluir jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <!--ruta archivo JavaScript-->
+    <script src="../../public/js/cat.js"></script>
+</head>
+<body>
 
 <div class="caja_popup" id="formregistrar">
-    <form class="contenedor_popup" onsubmit="event.preventDefault(); enviarFormulario();">
+    <form id="registroForm" class="contenedor_popup">
         <div class="container">
             <div class="d-flex justify-content-center">
                 <div class="row">
@@ -57,7 +29,7 @@
                                 <tr>
                                     <td colspan="2">
                                         <button type="button" class="btn btn-outline-primary" onclick="cancelarform()">Cancelar</button>
-                                        <button type="submit" class="btn btn-outline-primary" onclick="javascript:return confirm('¿Deseas registrar a esta categoria?');">Registrar</button>
+                                        <button type="submit" class="btn btn-outline-primary" onclick="return confirm('¿Deseas registrar a esta categoria?');">Registrar</button>
                                     </td>
                                 </tr>
                             </tbody>
@@ -68,3 +40,6 @@
         </div>
     </form>
 </div>
+
+</body>
+</html>
