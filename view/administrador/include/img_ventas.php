51776238042b0b6773b3ecd68d689d7fdccee9c5
tatanaldana <jbojhonatanaldana@gmail.com>
2024-03-18 13:47:24 -0500

    Update INCLUDE
    Se modifica los include para la visualización de imagen de las views de cada modulo

--- view/administrador/include/img_ventas.php
+++ view/administrador/include/img_ventas.php
@@ -4,10 +4,8 @@ echo "
 <div class='bienvenida'>
 <div class='position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center'>
   <div class='col-md-12 p-lg-5 mx-auto my-5'>
-  <div class='contenedor-busqueda' id='contenedor-busqueda'>
     <h1 class='display-3 fw-bold'>Ventas</h1>
   </div>
-</div>
 </div>
 </div>
 <div class='my-5'></div>";
