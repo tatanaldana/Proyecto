95ccd269fbeacd5ab9f504f2f5d180cece7f063b
DanielEduardo <daniele_gamboa@soy.sena.edu.co>
2024-03-18 19:22:24 -0500

    Se elimina codigo no necesario

--- view/public/js/usuario.js
+++ view/public/js/usuario.js
@@ -640,32 +640,6 @@ $(document).ready(function() {
     }
 });;
 
-<<<<<<< HEAD
-=======
-    productos.forEach(function(producto) {
-      var productoHTML = `
-          <div class="contenedor_productos row categoria" data-id_categoria="${producto.id_categoria}">
-              <form method="POST" action="../productos/carrito.php?accion=agregar&cod=${producto.cod}">
-                  <div>
-                      <div class="d-flex flex-column align-items-center">
-                          <img src="../../public/img/productos/${producto.nombre_pro}.jpeg" alt="${producto.nombre_pro}">
-                          <div style="padding-top:20px;font-size:18px;">${producto.nombre_pro}</div>
-                          <div style="padding-top:20px;font-size:18px;"><?php echo $productos_array[$i]["nombre_pro"]; ?></div>
-                          <div style="padding-top:10px;font-size:20px;"><?php echo "$" . $productos_array[$i]["precio_pro"]; ?></div>
-                          <div class="d-flex flex-column align-items-center">
-                              <input type="number" name="txtcantidad" value="1" size="1" class="mb-2" />
-                              <input type="submit" value="Agregar" style="background: var(--primario); color: white; border:none; padding:10px; width:100%;" />
-                              <div class="contenido">${producto.detalle}</div>
-                              <div class="precio">$${producto.precio_pro}</div>      
-                          </div>
-                      </div>
-                  </div>
-              </form>
-          </div>
-      `;
->>>>>>> dce3665cacae41f199be9a54e9af2fd35a5bf9a7
-
-
 
 
 //Funcion para mostrar los datos del perfil
