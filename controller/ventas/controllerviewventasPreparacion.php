44a47b6561ff381dff37be54ecec0b49b155650d
tatanaldana <jbojhonatanaldana@gmail.com>
2024-03-21 20:05:05 -0500

    Created controller view
    Se crean los controladores para la ejecución de las funciones de las vistas de la base de datos

---
+++ controller/ventas/controllerviewventasPreparacion.php
@@ -0,0 +1,18 @@
+<?php
+    # Incluimos la clase usuario
+    require_once('../../model/com_venta.php');
+     # Creamos un objeto de la clase usuario
+    $com_venta = new Com_venta();
+    # Llamamos al metodo  para realizar la consulta en la base de datos
+    $resultado = $com_venta->viewventas_preparacion();
+
+    if ($resultado) {
+        #GUARDAMOS LA VARIABLE EN FORMATO json PARA PODER ENVIAR LOS DATOS DE LA CONSULTA POR MEDIO DEL AJAX
+        $json_response = json_encode($resultado);
+        echo $json_response;
+    } else {
+        $error = array('error' => 'No se encontraron datos del usuario');
+        echo json_encode($error);
+    }
+
+?>
