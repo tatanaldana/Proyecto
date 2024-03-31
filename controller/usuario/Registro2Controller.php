81d84c6790cda54d1c66baab96b4c86283ef293f
tatanaldana <jbojhonatanaldana@gmail.com>
2024-03-31 17:10:10 -0500

    Update Registro2Controller.php
    Se modifica controller para poder hacer filtro de datos enviados al formulario, además de poder hacer la validación de seguridad de contraseña y finalmente manejo de errores para las situaciones que se pueden presentar al momento de registrarse

--- controller/usuario/Registro2Controller.php
+++ controller/usuario/Registro2Controller.php
@@ -3,51 +3,49 @@
 require_once('../../model/usuario.php');
 usuario::verificarSesion();
 
-  $name   = $_POST['name'];
-  $apellido   = $_POST['apellido'];
-  $email  = $_POST['email'];
-  $tel  = $_POST['tel'];
-  $genero  = $_POST['genero'];
-  $fecha_naci  = $_POST['fecha_naci'];
-  $tipo_doc = $_POST['tipo_doc'];
-  $doc = $_POST['doc'];
-  $fecha_reg = date ("y,m,d");
-  $direccion  = $_POST['direccion'];
-  $clave  = $_POST['clave'];
-  $clave2 = $_POST['clave2'];
+    $nombre = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $apellido = filter_var($_POST['apellido'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $tel = filter_var($_POST['tel'], FILTER_SANITIZE_STRING);
+    $genero = $_POST['genero']; // No es necesario filtrar ni sanitizar porque es una opción predefinida
+    $fecha_naci = filter_var($_POST['fecha_naci'], FILTER_SANITIZE_STRING);
+    $tipo_doc = $_POST['tipo_doc']; // No es necesario filtrar ni sanitizar porque es una opción predefinida
+    $doc = filter_var($_POST['doc'], FILTER_SANITIZE_STRING);
+    $fecha_reg = date("Y-m-d"); // No necesita ser filtrada ya que es generada internamente
+    $direccion = filter_var($_POST['direccion'], FILTER_SANITIZE_STRING);
+    $clave = filter_var($_POST['clave'], FILTER_SANITIZE_STRING);
+    $clave2 = filter_var($_POST['clave2'], FILTER_SANITIZE_STRING);
 
+    if (empty($email) || empty($clave) || empty($clave2) || empty($apellido)) {
+        echo 'error_1'; // Campos obligatorios, por favor llena el email y las claves
+    } else {
+        // Validar correo electrónico
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo 'error_2'; // Por favor ingresa un correo válido
+        } else {
+            // Validar contraseña (mínimo 8 caracteres alfanuméricos)
+            $caracteres_especiales_permitidos = '!@#$%^&*()-_+=[]{}|;:,.<>?';
+            if (strlen($clave) < 8 || 
+                !preg_match('/[A-Za-z]/', $clave) || 
+                !preg_match('/[0-9]/', $clave) || 
+                !preg_match('/[' . preg_quote($caracteres_especiales_permitidos, '/') . ']/', $clave)) {
+                echo 'error_3'; // La contraseña no cumple con los requisitos mínimos de seguridad
+            } else {
+                // Confirmar que las contraseñas coincidan
+                if ($clave !== $clave2) {
+                    echo 'error_4'; // Las claves deben ser iguales, por favor inténtalo de nuevo
+                } else {
+                    // Incluir la clase usuario
+                    require_once('../../model/usuario.php');
 
+                    // Crear un objeto de la clase usuario
+                    $usuario = new Usuario();
 
+                    // Llamar al método registroUsuario para guardar los datos en la base de datos
+                    $resultado = $usuario->registroUsuario2($nombre, $email, $clave, $tel, $apellido, $genero, $fecha_naci, $tipo_doc, $doc, $fecha_reg, $direccion);
 
-  if(empty($email) || empty($clave) || empty($clave2) || empty($apellido))
-  {
-
-    echo 'error_1'; // Un campo esta vacio y es obligatorio
-
-  }else{
-
-    if($clave == $clave2){
-
-      if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
-        # Incluimos la clase usuario
-        
-
-        # Creamos un objeto de la clase usuario
-        $usuario = new Usuario();
-
-        # Llamamos al metodo login para validar los datos en la base de datos
-        $usuario -> registroUsuario2($name, $email, $clave, $tel, $apellido, $genero, $fecha_naci, $tipo_doc, $doc, $fecha_reg, $direccion);
-
-
-        
-      }else{
-        echo 'error_4';
-      }
-
-
-    }else{
-      echo 'error_2';
+                }
+            }
+        }
     }
-
-  }
+?>
