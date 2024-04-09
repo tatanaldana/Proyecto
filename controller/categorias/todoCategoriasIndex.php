9d2c9811fae584f8f76f4566c7cccb6fb90da791
DanielEduardo <daniele_gamboa@soy.sena.edu.co>
2024-04-09 13:42:55 -0500

    Se agrega un controller unicamente para mostrar las categorias del index

---
+++ controller/categorias/todoCategoriasIndex.php
@@ -0,0 +1,18 @@
+<?php
+    # Incluimos la clase categorias
+    require_once('../../model/categorias.php');
+     # Creamos un objeto de la clase categorias
+    $categorias = new Categorias();
+    # Llamamos al metodo  para realizar la consulta en la base de datos
+    $resultado = $categorias->get_categorias();
+
+    if ($resultado) {
+        #GUARDAMOS LA VARIABLE EN FORMATO json PARA PODER ENVIAR LOS DATOS DE LA CONSULTAPOR MEDIO DEL AJAX#
+        $json_response = json_encode($resultado);
+        echo $json_response;
+    } else {
+        $error = array('error' => 'No se encontraron datos de la categoria');
+        echo json_encode($error);
+    }
+
+?>
